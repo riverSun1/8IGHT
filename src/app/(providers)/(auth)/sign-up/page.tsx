11a263908e43b464ac029a535a8b71afb724e118
LinQuixote <leein9404@naver.com>
2024-07-09 23:27:40 +0900

    feat : 로그인 화면 input 기능구현중

--- src/app/(providers)/(auth)/sign-up/page.tsx
+++ src/app/(providers)/(auth)/sign-up/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { useAuth } from "@/contexts/auth.context";
+import AuthInput from "../_components/Input";
 
 function SignUpPage() {
   const { signUp } = useAuth();
+  const router = useRouter();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
@@ -14,25 +17,42 @@ function SignUpPage() {
   };
 
   return (
-    <div className="max-w-96 flex flex-col">
-      <input
-        className="input"
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        className="input"
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className="mt-3 button" onClick={handleClickSignUp}>
-        가입하기
+    <>
+      <button className="fixed top-" onClick={() => router.back()}>
+        취소하고 돌아가기
       </button>
-    </div>
+      <h1 className="font-bold text-[28px] text-center text-neutral-950">
+        회원가입
+      </h1>
+      <div className="flex flex-col gap-[30px]">
+        <AuthInput
+          placeholder="이메일을 입력해주세요."
+          isEmail
+          labelText="이메일"
+          setInput={setEmail}
+        />
+        <div className="flex flex-col gap-[8px]">
+          <AuthInput
+            placeholder="비밀번호를 입력해주세요."
+            isPassword
+            labelText="비밀번호"
+            setInput={setPassword}
+          />
+          <AuthInput
+            placeholder="비밀번호를 다시 한번 입력해주세요."
+            isPasswordCheck
+            passwordNum={password}
+          />
+          <p className="text-neutral-500 text-[14px] font-light">
+            영문 대소문자, 숫자, 특수문자를 3가지 이상으로 조합해 8자 이상 16자
+            이하로 입력해주세요.
+          </p>
+        </div>
+        <button className="mt-3 button" onClick={handleClickSignUp}>
+          가입하기
+        </button>
+      </div>
+    </>
   );
 }
 
