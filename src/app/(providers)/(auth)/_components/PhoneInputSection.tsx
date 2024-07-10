de301bad3757ea0db2e7bd1c86aa31805d95f53a
LinQuixote <leein9404@naver.com>
2024-07-10 13:43:56 +0900

    feat : 기능 구현 중 잠시 저장

--- src/app/(providers)/(auth)/_components/PhoneInputSection.tsx
+++ src/app/(providers)/(auth)/_components/PhoneInputSection.tsx
@@ -1,21 +1,43 @@
 "use client";
 
-import { useId } from "react";
+import { useState } from "react";
+
+// 인증번호를 입력해주세요.
+// 올바르지 않은 인증번호입니다. 인증번호를 확인해주세요.
+// 유효시간 01:07
+// 인증 시간이 만료되었습니다. 다시 시도해 주세요.
 
 function PhoneInputSection() {
-  const id = useId();
+  const [initial, setInitial] = useState<boolean>(false);
+  const [nationalCode, setNationalConde] = useState();
+  const [phoneNum, setPhonNum] = useState<number | "">("");
+  const [accessCode, setAccessCode] = useState<number | "">("");
   return (
     <div className="">
-      <label className=" text-neutral-500" htmlFor={id}>
-        휴대폰 번호
-      </label>
+      <label className=" text-neutral-500">휴대폰 번호</label>
       <div className="flex flex-col gap-[8px]">
-        <input type="text" name="" id="" />
+        <input
+          className="text-base px-4 h-[50px] rounded-[10px] border border-neutral-300 outline-none  placeholder:font-normal placeholder:text-neutral-300"
+          type="text"
+        />
         <div>
-          <input type="text" name="" id="" />
+          <input
+            className="text-base px-4 h-[50px] rounded-[10px] border border-neutral-300 outline-none  placeholder:font-normal placeholder:text-neutral-300"
+            type="text"
+            placeholder="(예시) 01013245768"
+          />
+          {!initial ? (
+            <button>인증번호 받기</button>
+          ) : (
+            <button>인증번호 재전송</button>
+          )}
         </div>
         <div>
-          <input type="text" name="" id="" />
+          <input
+            className="text-base px-4 h-[50px] rounded-[10px] border border-neutral-300 outline-none placeholder:font-normal placeholder:text-neutral-300"
+            type="text"
+            placeholder="인증번호를 입력해주세요."
+          />
         </div>
         <p> </p>
       </div>
--- src/app/api/auth/code-global/route.ts
+++ src/app/api/auth/code-global/route.ts
@@ -1,6 +1,36 @@
-const END_POINT =
-  "https://api.odcloud.kr/api/15091117/v1/uddi:bbcc2939-88e0-4a54-af03-ab819b4130e6?page=1&perPage=10";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-  // NEXT_PUBLIC_PHONE_CODE_GLOBAL_ENCODING
+const END_POINT =
+  "https://api.odcloud.kr/api/15091117/v1/uddi:bbcc2939-88e0-4a54-af03-ab819b4130e6?page=1&perPage=196";
+
+type CodeGlobalObj = {
+  한글명: string;
+  영문명: string;
+  "ISO numeric": number;
+};
+type ApiResponse = {
+  data: CodeGlobalObj[];
+};
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    const response = await fetch(END_POINT, {
+      headers: {
+        Authorization: `Infuser ${process.env.NEXT_PUBLIC_PHONE_CODE_GLOBAL_ENCODING}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data: ApiResponse = await response.json();
+    return NextResponse.json(data.data);
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
