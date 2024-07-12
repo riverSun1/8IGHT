79adace7671466c56ced4cbdac1d03d57e690079
eunkyum jeon <silverkyum27@naver.com>
2024-07-12 09:55:59 +0900

    pull

--- src/app/(providers)/(auth)/layout.tsx
+++ src/app/(providers)/(auth)/layout.tsx
@@ -7,7 +7,7 @@ function AuthLayout({ children }: PropsWithChildren) {
     <>
       <AuthHeader />
       <div className="relative w-full h-full overflow-y-auto flex justify-center px-[10px]">
-        <div className="mt-20 min-w-[300px]  w-full">{children}</div>
+        <div className="mt-20 min-w-[300px] w-full">{children}</div>
       </div>
     </>
   );
