6b973bcf7b699732e078d720683665e466982b3c
LinQuixote <leein9404@naver.com>
2024-07-12 20:25:50 +0900

    fix : 타입 오류 해결

--- src/app/(providers)/(root)/new-resume/page.tsx
+++ src/app/(providers)/(root)/new-resume/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { createClient } from "@/supabase/client";
-import { v4 as uuidv4 } from "uuid";
 import { useAuth } from "@/contexts/auth.context";
+import { createClient } from "@/supabase/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { FormEvent, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Tables } from "../../../../../types/supabase";
 
 const supabase = createClient();
+
+type FormDataType = Tables<"resumes">;
 
 interface FormData {
   id: string;
@@ -45,7 +48,7 @@ const NewResumePage = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
 
-  const [formData, setFormData] = useState<FormData>({
+  const [formData, setFormData] = useState<Partial<FormDataType>>({
     id: "",
     title: "",
     email: "",
@@ -172,7 +175,7 @@ const NewResumePage = () => {
           <input
             type="text"
             name="title"
-            value={formData.title}
+            value={formData.title ? formData.title : ""}
             onChange={handleChange}
             className="w-full border border-gray-300 p-2 rounded placeholder-gray-400"
             placeholder="이력서 제목(필수)"
@@ -187,7 +190,7 @@ const NewResumePage = () => {
               <input
                 type="text"
                 name="name"
-                value={formData.name}
+                value={formData.name ? formData.name : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 placeholder="이름"
@@ -199,7 +202,7 @@ const NewResumePage = () => {
               <input
                 type="date"
                 name="birth_date"
-                value={formData.birth_date}
+                value={formData.birth_date ? formData.birth_date : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 required
@@ -209,7 +212,7 @@ const NewResumePage = () => {
               <label className="block text-gray-700 mb-1">성별</label>
               <select
                 name="gender"
-                value={formData.gender}
+                value={formData.gender ? formData.gender : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 required
@@ -226,7 +229,7 @@ const NewResumePage = () => {
               <input
                 type="email"
                 name="email"
-                value={formData.email}
+                value={formData.email ? formData.email : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 placeholder="이메일"
@@ -238,7 +241,7 @@ const NewResumePage = () => {
               <input
                 type="tel"
                 name="phone"
-                value={formData.phone}
+                value={formData.phone ? formData.phone : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 placeholder="전화번호"
@@ -250,7 +253,7 @@ const NewResumePage = () => {
               <input
                 type="text"
                 name="address"
-                value={formData.address}
+                value={formData.address ? formData.address : ""}
                 onChange={handleChange}
                 className="w-full border border-gray-300 p-2 rounded mb-2"
                 placeholder="주소"
@@ -263,7 +266,7 @@ const NewResumePage = () => {
         <Section label="간단 소개글">
           <textarea
             name="personalInfo"
-            value={formData.personalInfo}
+            value={formData.personalInfo ? formData.personalInfo : ""}
             onChange={handleChange}
             className="w-full border border-gray-300 p-2 rounded"
             placeholder="본인의 업무 경험을 기반으로 핵심역량과 업무 스킬을 간단히 작성해주세요. 3-5줄로 요약하여 작성하는 것을 추천합니다!"
@@ -271,15 +274,16 @@ const NewResumePage = () => {
         </Section>
 
         <Section label="경력">
-          {formData.career.map((career, index) => (
-            <textarea
-              key={index}
-              value={career}
-              onChange={(e) => handleChange(e, index, "career")}
-              className="w-full border border-gray-300 p-2 rounded mb-2"
-              placeholder="담당하신 업무 중 우선순위가 높은 업무를 선별하여 최신순으로 작성해주세요. 신입의 경우, 직무와 관련된 대외활동, 인턴, 계약직 경력 등이 있다면 작성해주세요. 업무 또는 활동 시 담당했던 역할과 과정, 성과에 대해 자세히 작성해주세요. 업무 성과는 되도록 구체적인 숫자 혹은 %로 표현해주세요! 커리어 조회 후 기업명이 실제와 다른 경우, 부서명/직책 란에 원하시는 기업명을 작성해주세요."
-            />
-          ))}
+          {formData.career &&
+            formData.career.map((career, index) => (
+              <textarea
+                key={index}
+                value={career}
+                onChange={(e) => handleChange(e, index, "career")}
+                className="w-full border border-gray-300 p-2 rounded mb-2"
+                placeholder="담당하신 업무 중 우선순위가 높은 업무를 선별하여 최신순으로 작성해주세요. 신입의 경우, 직무와 관련된 대외활동, 인턴, 계약직 경력 등이 있다면 작성해주세요. 업무 또는 활동 시 담당했던 역할과 과정, 성과에 대해 자세히 작성해주세요. 업무 성과는 되도록 구체적인 숫자 혹은 %로 표현해주세요! 커리어 조회 후 기업명이 실제와 다른 경우, 부서명/직책 란에 원하시는 기업명을 작성해주세요."
+              />
+            ))}
           <button
             type="button"
             onClick={() => handleAddField("career")}
@@ -290,15 +294,16 @@ const NewResumePage = () => {
         </Section>
 
         <Section label="학력">
-          {formData.education.map((edu, index) => (
-            <textarea
-              key={index}
-              value={edu}
-              onChange={(e) => handleChange(e, index, "education")}
-              className="w-full border border-gray-300 p-2 rounded mb-2"
-              placeholder="최신순으로 학력을 작성해주세요."
-            />
-          ))}
+          {formData.education &&
+            formData.education.map((edu, index) => (
+              <textarea
+                key={index}
+                value={edu}
+                onChange={(e) => handleChange(e, index, "education")}
+                className="w-full border border-gray-300 p-2 rounded mb-2"
+                placeholder="최신순으로 학력을 작성해주세요."
+              />
+            ))}
           <button
             type="button"
             onClick={() => handleAddField("education")}
@@ -309,15 +314,16 @@ const NewResumePage = () => {
         </Section>
 
         <Section label="스킬">
-          {formData.skills.map((skill, index) => (
-            <textarea
-              key={index}
-              value={skill}
-              onChange={(e) => handleChange(e, index, "skills")}
-              className="w-full border border-gray-300 p-2 rounded mb-2"
-              placeholder="개발 스택, 디자인 툴, 마케팅 툴 등 가지고 있는 직무와 관련된 스킬을 추가해보세요. 데이터 분석 툴이나 협업 툴 등의 사용해본 경험이 있으신 툴들도 추가해보세요."
-            />
-          ))}
+          {formData.skills &&
+            formData.skills.map((skill, index) => (
+              <textarea
+                key={index}
+                value={skill}
+                onChange={(e) => handleChange(e, index, "skills")}
+                className="w-full border border-gray-300 p-2 rounded mb-2"
+                placeholder="개발 스택, 디자인 툴, 마케팅 툴 등 가지고 있는 직무와 관련된 스킬을 추가해보세요. 데이터 분석 툴이나 협업 툴 등의 사용해본 경험이 있으신 툴들도 추가해보세요."
+              />
+            ))}
           <button
             type="button"
             onClick={() => handleAddField("skills")}
@@ -328,15 +334,16 @@ const NewResumePage = () => {
         </Section>
 
         <Section label="수상 및 기타">
-          {formData.awards.map((award, index) => (
-            <textarea
-              key={index}
-              value={award}
-              onChange={(e) => handleChange(e, index, "awards")}
-              className="w-full border border-gray-300 p-2 rounded mb-2"
-              placeholder="수상 이력, 직무 관련 자격증, 수료한 교육이나 참석한 외부활동 등이 있다면 간략히 작성해주세요. 지원하는 회사에서 요구하는 경우가 아니라면 운전면허증과 같은 자격증은 생략하는 것이 좋습니다!"
-            />
-          ))}
+          {formData.awards &&
+            formData.awards.map((award, index) => (
+              <textarea
+                key={index}
+                value={award}
+                onChange={(e) => handleChange(e, index, "awards")}
+                className="w-full border border-gray-300 p-2 rounded mb-2"
+                placeholder="수상 이력, 직무 관련 자격증, 수료한 교육이나 참석한 외부활동 등이 있다면 간략히 작성해주세요. 지원하는 회사에서 요구하는 경우가 아니라면 운전면허증과 같은 자격증은 생략하는 것이 좋습니다!"
+              />
+            ))}
           <button
             type="button"
             onClick={() => handleAddField("awards")}
@@ -347,16 +354,17 @@ const NewResumePage = () => {
         </Section>
 
         <Section label="링크">
-          {formData.links.map((link, index) => (
-            <input
-              key={index}
-              type="text"
-              value={link}
-              onChange={(e) => handleChange(e, index, "links")}
-              className="w-full border border-gray-300 p-2 rounded mb-2"
-              placeholder="깃헙, 노션으로 작성한 포트폴리오, 구글 드라이브 파일 등 업무 성과를 보여줄 수 있는 링크가 있다면 작성해주세요."
-            />
-          ))}
+          {formData.links &&
+            formData.links.map((link, index) => (
+              <input
+                key={index}
+                type="text"
+                value={link}
+                onChange={(e) => handleChange(e, index, "links")}
+                className="w-full border border-gray-300 p-2 rounded mb-2"
+                placeholder="깃헙, 노션으로 작성한 포트폴리오, 구글 드라이브 파일 등 업무 성과를 보여줄 수 있는 링크가 있다면 작성해주세요."
+              />
+            ))}
           <button
             type="button"
             onClick={() => handleAddField("links")}
