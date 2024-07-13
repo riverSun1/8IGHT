e409f6e150a7a3568f9bef5b53ca80d7e907ae0d
eunkyum jeon <silverkyum27@naver.com>
2024-07-13 19:50:28 +0900

    feat: 작성완료&첨부완료 추가, fix: 워크박스 날짜 위치 수정

--- src/app/(providers)/(auth)/_components/WorkBox.tsx
+++ src/app/(providers)/(auth)/_components/WorkBox.tsx
@@ -1,3 +1,59 @@
+// "use client";
+
+// import React from "react";
+// import { useRouter } from "next/navigation";
+// import Image from "next/image";
+
+// interface WorkBoxProps {
+//   id: string;
+//   title: string;
+//   date: string;
+//   onDelete: () => void;
+//   onEdit?: () => void;
+//   onTitleClick?: () => void;
+//   isFileUpload: boolean;
+//   fileURL?: string; // 파일 url추가
+// }
+
+// const WorkBox: React.FC<WorkBoxProps> = ({
+//   id,
+//   title,
+//   date,
+//   onDelete,
+//   onEdit,
+//   onTitleClick,
+//   isFileUpload,
+//   fileURL, // 파일 url 추가
+// }) => {
+//   const router = useRouter();
+
+//   const handleEditClick = () => {
+//     router.push(`/new-resume?id=${id}`);
+//   };
+
+//   return (
+//     <div className="relative border border-gray-300 bg-white p-4 rounded flex flex-col justify-between hover:shadow-lg hover:border-blue-500 transition duration-200 w-60 h-48">
+//       <div className="cursor-pointer" onClick={handleEditClick}>
+//         <div className="text-lg font-semibold mb-2">{title}</div>
+//       </div>
+//       <div className="text-gray-500 mt-4">{date}</div>
+//       <div
+//         className="absolute bottom-4 right-2 cursor-pointer"
+//         onClick={onDelete}
+//       >
+//         <Image
+//           src="/delete-icon.png"
+//           alt="Delete Icon"
+//           width={24}
+//           height={24}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkBox;
+
 "use client";
 
 import React from "react";
@@ -12,7 +68,6 @@ interface WorkBoxProps {
   onEdit?: () => void;
   onTitleClick?: () => void;
   isFileUpload: boolean;
-  fileURL?: string; // 파일 url추가
 }
 
 const WorkBox: React.FC<WorkBoxProps> = ({
@@ -23,22 +78,28 @@ const WorkBox: React.FC<WorkBoxProps> = ({
   onEdit,
   onTitleClick,
   isFileUpload,
-  fileURL, // 파일 url 추가
 }) => {
   const router = useRouter();
 
   const handleEditClick = () => {
-    router.push(`/new-resume?id=${id}`);
+    if (onEdit) {
+      onEdit();
+    } else {
+      router.push(`/new-resume?id=${id}`);
+    }
   };
 
   return (
     <div className="relative border border-gray-300 bg-white p-4 rounded flex flex-col justify-between hover:shadow-lg hover:border-blue-500 transition duration-200 w-60 h-48">
       <div className="cursor-pointer" onClick={handleEditClick}>
-        <div className="text-lg font-semibold mb-2">{title}</div>
+        <div className="text-lg font-semibold mb-2 line-clamp-2">{title}</div>
+        <div className="text-gray-500 mb-2">{date}</div>
       </div>
-      <div className="text-gray-500 mt-4">{date}</div>
+      <div className="absolute bottom-2 left-4 text-gray-500">
+        {isFileUpload ? "첨부 완료" : "작성 완료"}
+      </div>
       <div
-        className="absolute bottom-3 right-2 cursor-pointer"
+        className="absolute bottom-2 right-2 cursor-pointer"
         onClick={onDelete}
       >
         <Image
